c9ccbeaf2d8cc0a892932dec8ed9a6343ff208f1
William Rutherford <william@williamrutherford.org>
2026-02-14 21:12:33 -0500

    change other functions to use KernelDensityEstimatorGeneric

--- KernelDensityEstimator.py
+++ KernelDensityEstimator.py
@@ -3,6 +3,7 @@ import numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from KernelDensityEstimatorGeneric import GenericGaussEstimator
 
 '''
 A single Gaussian Normal Distribution density function. 
@@ -25,40 +26,12 @@ evaluation_points, density_estimation
 evaluation_pts has shape (divs,) representing the points it was evaluated at
 density_estimation has shape (divs,) representing the relative probability density at each evaluated point
 '''
-def discrete_estimator(data_pts : npt.ArrayLike, bandwidth : float = 1.0, divs : int = 100, bounds_factor : float = 0.25, bounds_abs = None, input_pts = None):
-    # For each data point, we have a normal distribution offset by it's value
-    # We then plug in a bunch of values, and sum over the result of the PDF
-    # For each point we evaluate, we only care about the distance between it and another point (the supposed 'center') meaning we can do pdf(other_pt - curr_pt)
-    if(bounds_abs is None):
-        data_range = np.max(data_pts) - np.min(data_pts)
-        lower_bnd = np.min(data_pts) - bounds_factor * data_range
-        upper_bnd = np.max(data_pts) + bounds_factor * data_range
-    else:
-        lower_bnd = bounds_abs[0]
-        upper_bnd = bounds_abs[1]
-    if(input_pts is None):
-        eval_pts = np.linspace(lower_bnd, upper_bnd, divs)
-    else:
-        eval_pts = input_pts
-    # let n be the number of data_pts, or len(data_pts)
-    
-    # data has shape (n,)
-    # data_stretched has shape (n, divs) where data_stretched[:, i] = data_pts
-    data_stretched = np.column_stack(divs * (data_pts,))
-    # eval has shape (divs,)
-    # eval_stretched has shape (n, divs) where eval_stretched[i, :] = eval_pts
-    eval_stretched = np.tile(eval_pts, (len(data_pts), 1))
-    
-    dist_diff = data_stretched - eval_stretched
-    # For each offset, replace with the probability from the gaussian pdf. 
-    norm_func = gaussian_fn(center = 0.0, std_dev = bandwidth) 
-    # Generate the probability distribution for each offset
-    ind_density = norm_func(dist_diff)
-    # Sum up each column, representing the total density for each point sampled. 
-    tot_density = np.sum(ind_density, axis = 0)
-    # Then, we make it relative to the whole area; giving us a probability density estimation (the area underneath is 1)
-    relative_density = tot_density / np.sum(tot_density)
-    return eval_pts, relative_density, bandwidth
+def KernelDensityEstimator(data_pts : npt.ArrayLike, bandwidth : float = 1.0, divs : int = 100, bounds_factor : float = 0.25, bounds_abs = None, input_pts = None):
+    pass
+    fn = gaussian_fn(center = 0.0, std_dev = bandwidth)
+    result = GenericGaussEstimator(data_pts = data_pts, fn = fn, divs = divs, bounds_factor = bounds_factor, bounds_abs = bounds_abs, input_pts = input_pts)
+    return result + (bandwidth,)
+
 
 '''
 A Kernel Density Estimator which can be constantly updated with new data.
@@ -122,7 +95,7 @@ class UpdatingKernelDensityEstimator:
 
 if(__name__ == "__main__"):
     bimodal_test_data = np.concatenate((np.random.normal(loc = -3, scale = 0.7, size = 10), np.random.normal(loc = 5, scale = 1.15, size = 10)))
-    test_gauss = discrete_estimator(data_pts = bimodal_test_data)
+    test_gauss = KernelDensityEstimator(data_pts = bimodal_test_data)
     
     # Plot the results
     eval_pts, relative_density, std_dev = test_gauss
--- KernelDensityEstimatorSmooth.py
+++ KernelDensityEstimatorSmooth.py
@@ -3,62 +3,23 @@ import numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-
-'''
-A single Gaussian Normal Distribution density function. 
-*Should* use numpy methods and return a function, so it can operate on a whole array of elements. 
-'''
-def gaussian_fn(center = 0.0, std_dev = 1.0):
-    return (lambda x : 1 / (std_dev * math.sqrt(2 * math.pi)) * np.exp((-1/2) * np.power((x - center) /std_dev, 2))) 
-
+from KernelDensityEstimator import KernelDensityEstimator, gaussian_fn
 
 '''
 A Kernel Density Estimator where no bandwidth is provided; we simply choose the bandwidth which is the most 'smooth'
 '''
 def smoothGaussEstimator(data_pts : npt.ArrayLike, divs : int = 100, bounds_factor : float = 0.25, bounds_abs = None, input_pts = None):
-    
-    if(input_pts is None):
-        if(bounds_abs is None):
-            data_range = np.max(data_pts) - np.min(data_pts)
-            lower_bnd = np.min(data_pts) - bounds_factor * data_range
-            upper_bnd = np.max(data_pts) + bounds_factor * data_range
-        else:
-            lower_bnd = bounds_abs[0]
-            upper_bnd = bounds_abs[1]
-        eval_pts = np.linspace(lower_bnd, upper_bnd, divs)
-    else:
-        eval_pts = input_pts
-
-
     # calculation of average distance between consecutive points
     sorted_pts = np.sort(data_pts)
     pt_distances = np.abs(sorted_pts[1:] - sorted_pts[:-1])
     pt_dist_avg = np.mean(pt_distances)
     # whatever that is, use it as as the standard deviation
     std_dev = pt_dist_avg * 0.75
+    fn = gaussian_fn(center = 0.0, std_dev = std_dev)
+    result = KernelDensityEstimator(data_pts = data_pts, bandwidth = std_dev, divs = divs, bounds_factor = bounds_factor, bounds_abs = bounds_abs, input_pts = input_pts)
+    return result
 
 
-
-    # let n be the number of data_pts, or len(data_pts)
-    
-    # data has shape (n,)
-    # data_stretched has shape (n, divs) where data_stretched[:, i] = data_pts
-    data_stretched = np.column_stack(divs * (data_pts,))
-    # eval has shape (divs,)
-    # eval_stretched has shape (n, divs) where eval_stretched[i, :] = eval_pts
-    eval_stretched = np.tile(eval_pts, (len(data_pts), 1))
-    
-    dist_diff = data_stretched - eval_stretched
-    # For each offset, replace with the probability from the gaussian pdf. 
-    norm_func = gaussian_fn(center = 0.0, std_dev = std_dev) 
-    #vec_norm_func = np.vectorize(norm_func)
-    # Generate the probability distribution for each offset
-    ind_density = norm_func(dist_diff)
-    # Sum up each column, representing the total density for each point sampled. 
-    tot_density = np.sum(ind_density, axis = 0)
-    # Then, we make it relative to the whole area; giving us a probability density estimation (the area underneath is 1)
-    relative_density = tot_density / np.sum(tot_density)
-    return eval_pts, relative_density, std_dev
 
 if(__name__ == "__main__"):
     bimodal_test_data = np.concatenate((np.random.normal(loc = -3, scale = 0.7, size = 10), np.random.normal(loc = 5, scale = 1.15, size = 10)))
